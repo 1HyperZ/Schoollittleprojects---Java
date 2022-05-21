45033027edde13a4912009c9935217d4aae4a37a
1HyperZ <omerzur06@gmail.com>
2022-05-21 20:44:28 +0300

    aviv

---
+++ src/testing.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class testing
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int num1 = 1;
+        int num2 = 2;
+        boolean b = (num1<num2);
+        System.out.println(b);
+    }
+} 
